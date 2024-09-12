a4548fc2965a5551c269d4a1e287c1a06ba130bf
asif <asif69284@gmail.com>
2024-09-12 11:52:48 +0545

    vitest/config instead of vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
+    environment: 'jsdom',
     globals: true,
   },
   plugins: [react()],
