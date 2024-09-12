f0645a74301ba3141d021c4d8a9697431aa04335
asif <asif69284@gmail.com>
2024-09-12 11:12:34 +0545

    removed

--- src/pages/FootballTablesd.tsx
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-// import FootballTable from "./FootballTable";
-
-// test('renders football table data', () => {
-//     render(<FootballTable />)
-//     const tableData = screen.getByText(/Manchester City/i)
-//     expect(tableData)
-// })
