3e2ac87681a3da9eb568074ebab04a756ece7914
asif <asif69284@gmail.com>
2024-09-18 15:53:00 +0545

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  }
 })
