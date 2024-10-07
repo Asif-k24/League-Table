961bb5a5cc24522def607471d3576741304b08c4
asif <asif69284@gmail.com>
2024-10-07 17:30:21 +0545

    Added match and league type

--- src/redux/slice/dataSlice.ts
+++ src/redux/slice/dataSlice.ts
@@ -1,12 +1,27 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import CalculateLeagueTable, { TeamDetails } from "../../components/CalculateLeagueTable";
-import { League } from "../../components/CalculateLeagueTable"
 
 interface DataState {
     loading: boolean;
     data: League | null;
     processedData: TeamDetails[] | null; // Adjusting the type as needed based on CalculateLeagueTable's return type
     error: string | null;
+}
+
+export interface Match {
+    // Define the structure of the match data according to the API response
+    round: string;
+    date: string;
+    team1: string;
+    team2: string;
+    score?: {
+        ft: number[];
+    };
+}
+
+export interface League {
+    name: string;
+    matches: Match[];
 }
 
 export const fetchData = createAsyncThunk<League>('fetchData', async () => {
